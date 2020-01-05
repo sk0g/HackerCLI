799ee6c4ae7704b77e27cc817d322d101763a98e
sk0g <ani.mehta@mail.com>
2020-01-05 18:12:39 +1000

    Add getDetailsForPost()

--- update.go
+++ update.go
@@ -5,12 +5,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const (
 	newStoriesUrl  = "https://hacker-news.firebaseio.com/v0/newstories.json"
 	topStoriesUrl  = "https://hacker-news.firebaseio.com/v0/topstories.json"
 	bestStoriesUrl = "https://hacker-news.firebaseio.com/v0/beststories.json"
+
+	itemDetailsUrl = "https://hacker-news.firebaseio.com/v0/item/$1.json"
 )
 
 // get HN post IDs, given a URL to query for the array of post ID ints
@@ -27,6 +31,28 @@ func getHNPostIDs(url string) (postIDs []int) {
 	}
 
 	err = json.Unmarshal(responseBody, &postIDs)
+	if err != nil {
+		log.Fatal("Error unmarshalling JSON: ", err)
+	}
+
+	return
+}
+
+func getDetailsForPost(postID int) (postData Data) {
+	url := strings.Replace(itemDetailsUrl, "$1", strconv.Itoa(postID), 1)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Error querying for post: ", err)
+		return
+	}
+
+	responseBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error reading post details: ", err)
+	}
+
+	err = json.Unmarshal(responseBody, &postData)
 	if err != nil {
 		log.Fatal("Error unmarshalling JSON: ", err)
 	}
