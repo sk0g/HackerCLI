f60d8bc6dbed696fa7f5648e2f21198875f6b2be
sk0g <ani.mehta@mail.com>
2020-01-05 18:15:00 +1000

    Add initialisation logic in main.go

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
 func main() {
+	postIDs := getHNPostIDs(bestStoriesUrl)
+
+	for i := 0; i <= 5 && i < len(postIDs); i++ {
+		go func() {
+			postDetails := getDetailsForPost(postIDs[i])
+
+			// TODO: display post
+		}()
+	}
 }
